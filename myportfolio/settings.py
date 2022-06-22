b339c7e8a8c034283e8edbc5e9fc122995611ae8
django <78250295+iamsulavpariyar@users.noreply.github.com>
2022-06-22 06:31:00 +0545

    first commit

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@hje&qg-@-#yyig@ed5dcklu0f18m$8cflllk%7i-qpqz+^=jg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://sulavpariyar.herokuapp.com/', 'localhost', '127.0.0.1:8000']
 
 
 # Application definition
